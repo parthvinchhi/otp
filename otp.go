1965d2a4bc83892abe763efe2a0523fa021584fb
parthvinchhi <parthvinchhi21112000@gmail.com>
2026-01-21 16:21:09 +0530

    added send mail function

--- otp.go
+++ otp.go
@@ -1,6 +1,8 @@
 package otp
 
 import (
+	"fmt"
+	"net/smtp"
 	"time"
 
 	"math/rand"
@@ -27,4 +29,36 @@ func GenerateOtp(n int) int {
 
 	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(max-min+1) + min
+}
+
+type EmailConfig struct {
+	From       string
+	Password   string
+	SMTPServer string
+	SMTPPort   string
+}
+
+func NewEmailConfig(from, password, smtpServer, smtpPort string) *EmailConfig {
+	return &EmailConfig{
+		From:       from,
+		Password:   password,
+		SMTPServer: smtpServer,
+		SMTPPort:   smtpPort,
+	}
+}
+
+func (cfg *EmailConfig) SendEmail(to []string, otp int) error {
+	msg := fmt.Sprintf("Subject : OTP Verification\n\n Your OTP is : %d", otp)
+
+	auth := smtp.PlainAuth("", cfg.From, cfg.Password, cfg.SMTPServer)
+
+	err := smtp.SendMail(
+		fmt.Sprintf("%s:%s", cfg.SMTPServer, cfg.SMTPPort),
+		auth,
+		cfg.From,
+		to,
+		[]byte(msg),
+	)
+
+	return err
 }
